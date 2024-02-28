32e51b3a77ca69a3c52bef076f568d7c94a3da3d
Burak Yavuzalp <B.Yavuzalp@TKI-Chemnitz.de>
2024-02-28 09:30:11 +0100

    add StocksDetail

---
+++ StocksApp/StocksDetail.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StocksApp
+{
+    /// <summary>
+    /// Interaktionslogik für StocksDetail.xaml
+    /// </summary>
+    public partial class StocksDetail : Window
+    {
+        public StocksDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Your navigation code to go back to the previous view goes here.
+            // For a simple scenario, if this is a Window, you might just close this window:
+            this.Close();
+
+            // If you're using a navigation service or a more complex navigation structure, you would call that service/method here instead.
+        }
+
+    }
+}
